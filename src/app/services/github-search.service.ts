c0ee679383a18c7877a6ae57dec7f6ee1a0d9d15
dennis <machariad196@gmail.com>
2024-11-04 15:27:28 +0300

    create component and service

---
+++ src/app/services/github-search.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+const api = 'https://api.github.com'
+@Injectable({
+  providedIn: 'root'
+})
+export class GithubSearchService {
+
+  private token = environment.githubToken
+  
+  constructor(private http: HttpClient) { }
+
+  headers = new HttpHeaders({
+    'Authorization': `token ${this.token}`
+  });
+
+
+
+  getUserData(userName: string){
+    const headers = new HttpHeaders({
+      'Authorization': `token ${this.token}`
+    });
+    return this.http.get(api+'/users/' + userName , { headers });
+  }
+  getRepositories(userName:string, range:string){
+       const headers = new HttpHeaders({
+      'Authorization': `token ${this.token}`
+    });
+    return this.http.get(api+'/users/' + userName+'/repos?per_page='+range, { headers });
+  }
+  getLanguages(userName:string,repoName:string){
+       const headers = new HttpHeaders({
+      'Authorization': `token ${this.token}`
+    });
+    return this.http.get(api+'/repos/'+userName+'/'+repoName+'/languages', { headers });
+  }
+  getFollowers(userName:string){
+       const headers = new HttpHeaders({
+      'Authorization': `token ${this.token}`
+    });
+    return this.http.get(api+'/users/'+userName+'/followers', { headers })
+  }
+  getFollowing(userName:string){
+       const headers = new HttpHeaders({
+      'Authorization': `token ${this.token}`
+    });
+    return this.http.get(api+'/users/'+userName+'/following', { headers })
+  }
+
+}
